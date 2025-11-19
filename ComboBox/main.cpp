43ad861c7d2995b605839b2c7751ca567d2981c2
Андрей Полищук <dcnfandrey@mail.ru>
2025-11-19 19:21:15 +0300

    IDOK DONE

--- ComboBox/main.cpp
+++ ComboBox/main.cpp
@@ -25,7 +25,7 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)g_sz_VALUES[i]);
 		}
 	}
-		break;
+	break;
 	case WM_COMMAND:
 	{
 		switch (LOWORD(wParam))
@@ -43,27 +43,34 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 			//Gets a string from a list in a combo box.
 			//Send the CB_GETLBTEXT message explicitly.
-			SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_buffer);
-
 			//Создаем ответ для отправки в MessageBox
 			CHAR sz_answer[SIZE] = {};
-			//заголовочный файл stdio.h
-			//Для использования без предупреждений пришлось добавить _CRT_SECURE_NO_WARNINGS в свойствах проекта
-			//C/C++/Preprocessor/Preprocessor Definition/_CRT_SECURE_NO_WARNINGS <different options>
-			//Write formatted data to a string.
-			sprintf(sz_answer, "Выбран элемент \"%s\", номер %d", sz_buffer, i); //Используем С-шную функцию для помещения
-			//в сообщение текста, номера элемента и содержимого элемента
+			if (i != CB_ERR)
+			{
+				SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_buffer);
 
-			//Вывожу окончательный результат на экран
-			MessageBox(hWnd, sz_answer, "Info", MB_OK | MB_ICONINFORMATION);
+				//заголовочный файл stdio.h
+				//Для использования без предупреждений пришлось добавить _CRT_SECURE_NO_WARNINGS в свойствах проекта
+				//C/C++/Preprocessor/Preprocessor Definition/_CRT_SECURE_NO_WARNINGS <different options>
+				//Write formatted data to a string.
+				sprintf(sz_answer, "Выбран элемент \"%s\", номер %d", sz_buffer, i); //Используем С-шную функцию для помещения
+				//в сообщение текста, номера элемента и содержимого элемента
+
+				//Вывожу окончательный результат на экран
+				MessageBox(hWnd, sz_answer, "Info", MB_OK | MB_ICONINFORMATION);
+			}
+			else
+			{
+				MessageBox(hWnd, "Вы ничего не выбрали", "Info", MB_OK | MB_ICONINFORMATION);
+			}
 		}
-			break;
+		break;
 		case IDCANCEL:
 			EndDialog(hWnd, 0);
 			break;
 		}
 	}
-		break;
+	break;
 	case WM_CLOSE:
 		EndDialog(hWnd, 0);
 		break;
