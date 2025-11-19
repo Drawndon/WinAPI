8aaa72d2fccb222006428fe82abc4ae4e82fc9c7
Андрей Полищук <dcnfandrey@mail.ru>
2025-11-19 18:39:35 +0300

    IDOK DONE

--- ComboBox/main.cpp
+++ ComboBox/main.cpp
@@ -1,4 +1,6 @@
-#include<Windows.h>
+﻿#include<Windows.h>
+#include<stdio.h>
+#include<windowsx.h>
 #include"resource.h"
 
 CONST CHAR* g_sz_VALUES[] = { "This", "is", "my", "first", "Combo", "Box" };
@@ -29,12 +31,37 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		switch (LOWORD(wParam))
 		{
 		case IDOK:
+		{
+			HWND hCombo = GetDlgItem(hWnd, IDC_COMBO1); //Получаем ручку для IDC_COMBO1
+			//Gets the index of the currently selected item in a combo box.
+			//Send the CB_GETCURSEL message explicitly.
+			int i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+
+			//Создаем временный буфер для хранения текста
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+
+			//Gets a string from a list in a combo box.
+			//Send the CB_GETLBTEXT message explicitly.
+			SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_buffer);
+
+			//Создаем ответ для отправки в MessageBox
+			CHAR sz_answer[SIZE] = {};
+			//заголовочный файл stdio.h
+			//Для использования без предупреждений пришлось добавить _CRT_SECURE_NO_WARNINGS в свойствах проекта
+			//C/C++/Preprocessor/Preprocessor Definition/_CRT_SECURE_NO_WARNINGS <different options>
+			//Write formatted data to a string.
+			sprintf(sz_answer, "Выбран элемент \"%s\", номер %d", sz_buffer, i); //Используем С-шную функцию для помещения
+			//в сообщение текста, номера элемента и содержимого элемента
+
+			//Вывожу окончательный результат на экран
+			MessageBox(hWnd, sz_answer, "Info", MB_OK | MB_ICONINFORMATION);
+		}
 			break;
 		case IDCANCEL:
 			EndDialog(hWnd, 0);
 			break;
 		}
-
 	}
 		break;
 	case WM_CLOSE:
