5156f2a65ed5874f15d688bf8a3f2d9300ba7f68
Андрей Полищук <dcnfandrey@mail.ru>
2025-12-17 19:20:20 +0300

    My theme DONE

--- Calc/ColorsAndSkins.h
+++ Calc/ColorsAndSkins.h
@@ -12,4 +12,4 @@ CONST COLORREF g_clr_COLORS[][3] =
 	{RGB(150, 150, 150), RGB(50, 50, 50), RGB(0, 255, 0)},
 };
 
-CONST CHAR* g_sz_SKIN[] = { "square_blue", "metal_mistral" };
+CONST CHAR* g_sz_SKIN[] = { "square_blue", "metal_mistral", "my_theme"};
